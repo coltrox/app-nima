93998f9057d991f84dfcbda12be217ad68134f99
Pedro Coltro 👾 <138863769+coltrox@users.noreply.github.com>
2026-06-25 22:18:20 -0300

    feature: authService: api_url=localhost

--- src/screens/Auth/authService.js
+++ src/screens/Auth/authService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'http://192.168.0.247:3000/api/auth';
+const API_URL = 'http://localhost:3000/api/auth';
 
 /**
  * Função interna baseada na fórmula do Módulo 11 para validação real de CPF.
